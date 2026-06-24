ef272794f7a63c8255b1a0c905017330d469e3cd
kabhiram08 <kabhiram824@gmail.com>
2026-06-25 01:15:36 +0530

    Added Mid Labs Game

---
+++ Mid labs game.py
@@ -0,0 +1,9 @@
+character1 = input("Enter your name: ")
+object_found = input("Enter an object you see nearby:")
+character2 = input("Enter your friend's name: ")
+
+print("One day when " + character1 + " was walking through the park, saw a " + object_found + " lying on the ground. He decided to pick it up and take it to " + character2 + "'s house. When he arrived, " + character2 + " was very angry and threw it outside. ")
+
+# Final learning: Take different inputs from the user and store them in variables.
+# Use those variables inside a story by joining text and values together.
+# This helps create a simple Mad Libs game using input(), variables, and print().
